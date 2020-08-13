b71fdded14908321f3aa8729403c6c8f69a32dce
Brice Christian <bricechristian93@gmail.com>
2020-08-13 01:50:42 -0400

    Integrated recurring events with momentJS

--- src/components/Events/Calendar.jsx
+++ src/components/Events/Calendar.jsx
@@ -1,22 +1,63 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import Calendar from "react-calendar"
 import Moment from "moment"
-import moment from "moment-recur"
+// import moment from "moment-recur"
 
 import "react-calendar/dist/Calendar.css"
 import "./Calendar.module.scss"
 
+const parseDate = str => {
+  let mdy = str.split("/")
+  return new Date(mdy[2], mdy[0] - 1, mdy[1])
+}
+
+const dateDiffByWeek = (first, second) => {
+  // Take the difference between the dates and divide by milliseconds per day.
+  // Round to nearest whole number to deal with DST.
+  return Math.round((second - first) / (1000 * 60 * 60 * 24) / 7)
+}
+
 const EventsCalendar = props => {
   const { data } = props
+  const events = data
   const [date, setDate] = useState(new Date())
-  const formattedDate = new Moment(date)
-  const myDate = formattedDate.format("MM/DD/YYYY")
-  console.log(
-    moment().recur({
-      start: "01/01/2021",
-      end: "01/01/2022",
+
+  useEffect(() => {
+    //handles recurring events on mount
+    const initialRecurringArr = data.filter(({ node }) => {
+      return node.recurring_event
     })
-  )
+    initialRecurringArr.map(({ node }) => {
+      const initialStartDate = new Moment(node.start_date)
+      const initialEndDate = new Moment(node.end_date)
+      const formattedInitialStartDate = initialStartDate.format("MM/DD/YYYY")
+      const yearFromStart = node.recurring_end_date
+        ? new Moment(node.recurring_end_date).format("MM/DD/YYYY")
+        : initialStartDate.add(1, "years").format("MM/DD/YYYY")
+      const startAndYearDiff = dateDiffByWeek(
+        parseDate(formattedInitialStartDate),
+        parseDate(yearFromStart)
+      )
+      for (let i = 1; i <= startAndYearDiff; i++) {
+        const { title, recurring_frequency, description } = node
+        events.push({
+          node: {
+            title: title,
+            start_date: initialStartDate
+              .add(recurring_frequency, "d")
+              .toISOString()
+              .replace(".000", ""),
+            end_date: initialEndDate
+              .add(recurring_frequency, "d")
+              .toISOString()
+              .replace(".000", ""),
+            description: description,
+          },
+        })
+      }
+    })
+    console.log(events)
+  }, [])
 
   return (
     <section className="calendar pad">
@@ -33,7 +74,7 @@ const EventsCalendar = props => {
             const newDayZero = day < 10 ? "0" + day : day
             const dotw = date.getDay()
             const tileDate = `${year}-${newMonthZero}-${newDayZero}`
-            return data.map(({ node }, index) => {
+            return events.map(({ node }, index) => {
               const eventStartDate = node.start_date.split("T")[0]
               // console.log(tileDate);
               switch (eventStartDate) {
